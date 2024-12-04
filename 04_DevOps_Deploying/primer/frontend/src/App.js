ba6d47dd80c389f6d7834c7b04c6fa1388fc0d21
HlisTilen <tilen.hlis@proton.me>
2024-12-04 16:45:05 +0100

    fix: sreda

--- 04_DevOps_Deploying/primer/frontend/src/App.js
+++ 04_DevOps_Deploying/primer/frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          RIRS DEMO Deploy december 2024
+          RIRS DEMO Deploy december 2024 sreda
         </p>
         <a
           className="App-link"
